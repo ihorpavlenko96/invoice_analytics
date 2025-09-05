fb1f8404ced7ece838b24c6bf3b2bc86c31478c9
Andrii Aleksandruk <andrii.aleksandruk@honeycombsoft.com>
2025-09-05 15:32:07 +0300

    Refactor ESLint configuration to remove deprecated rules and simplify the rules object. Update build script in package.json to remove TypeScript compilation step, streamlining the build process.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -17,27 +17,6 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-        },
-      ],
-      // Prettier-friendly rules
-      'no-mixed-spaces-and-tabs': 'off',
-      'no-trailing-spaces': 'warn',
-      'no-multiple-empty-lines': ['warn', { max: 2, maxEOF: 1 }],
-      quotes: ['warn', 'single', { avoidEscape: true }],
-      semi: ['warn', 'always'],
-      'comma-dangle': ['warn', 'always-multiline'],
-    },
+    rules: {},
   },
 );
