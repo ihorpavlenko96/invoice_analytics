cb4ebbf89cfd512355aaf6a2f5698ce63a89cb49
ihorpavlenko96 <ihorpavlenko96@gmail.com>
2025-09-14 13:27:30 +0300

    Revert "IA-37: Add filters to User Management"

--- client/src/modules/users/stores/userManagementStore.ts
+++ client/src/modules/users/stores/userManagementStore.ts
@@ -1,16 +1,6 @@
 import { create } from 'zustand';
 import { User } from '../types/user';
 
-interface UserFilters {
-  email: string;
-  name: string;
-  tenant: string;
-  roles: string[];
-  status: 'all' | 'active' | 'inactive';
-  dateFrom: string;
-  dateTo: string;
-}
-
 interface UserManagementState {
   isFormOpen: boolean;
   selectedUser: User | null;
@@ -18,7 +8,6 @@ interface UserManagementState {
   userToDeleteId: string | null;
   isConfirmToggleStatusDialogOpen: boolean;
   userToToggleStatus: User | null;
-  filters: UserFilters;
 
   openCreateForm: () => void;
   openEditForm: (user: User) => void;
@@ -31,20 +20,7 @@ interface UserManagementState {
   openConfirmToggleStatusDialog: (user: User) => void;
   closeConfirmToggleStatusDialog: () => void;
   resetToggleStatusState: () => void;
-
-  updateFilters: (filters: Partial<UserFilters>) => void;
-  clearFilters: () => void;
 }
-
-const initialFilters: UserFilters = {
-  email: '',
-  name: '',
-  tenant: '',
-  roles: [],
-  status: 'all',
-  dateFrom: '',
-  dateTo: '',
-};
 
 export const useUserManagementStore = create<UserManagementState>((set) => ({
   // Initial state
@@ -54,7 +30,6 @@ export const useUserManagementStore = create<UserManagementState>((set) => ({
   userToDeleteId: null,
   isConfirmToggleStatusDialogOpen: false,
   userToToggleStatus: null,
-  filters: initialFilters,
 
   openCreateForm: (): void => set({ isFormOpen: true, selectedUser: null }),
   openEditForm: (user: User): void => set({ isFormOpen: true, selectedUser: user }),
@@ -72,8 +47,4 @@ export const useUserManagementStore = create<UserManagementState>((set) => ({
     set({ isConfirmToggleStatusDialogOpen: false, userToToggleStatus: null }),
   resetToggleStatusState: (): void =>
     set({ isConfirmToggleStatusDialogOpen: false, userToToggleStatus: null }),
-
-  updateFilters: (filters: Partial<UserFilters>): void =>
-    set((state) => ({ filters: { ...state.filters, ...filters } })),
-  clearFilters: (): void => set({ filters: initialFilters }),
 }));
