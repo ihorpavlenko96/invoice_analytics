3b9856b7da9dc90a7e9b7eb97665f19d5f86f137
Beezi AI <ai-bot@beezi.ai>
2025-10-30 09:43:22 +0000

    feat: Update: use this #FF69B4 specific color

--- client/src/themes/darkTheme.ts
+++ client/src/themes/darkTheme.ts
@@ -39,7 +39,7 @@ export const darkTheme = createTheme({
     invoiceStatus: {
       paid: '#C71585',
       unpaid: '#FFC0CB',
-      overdue: '#FF1493',
+      overdue: '#FF69B4',
     },
     divider: 'rgba(255, 255, 255, 0.12)',
   },
