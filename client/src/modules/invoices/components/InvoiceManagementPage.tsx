4a96bf19f16c3a945c861f56d864f47edf5e2364
Bohdan Krasiuk <bohdan.krasyuk@honeycombsoft.com>
2025-11-26 21:00:27 +0200

    Revert "feat: Currently, when a user tries to export invoices, only the invoices visible on a page are exported. (#213)"
    This reverts commit 334d655bc57ed4bbc3c29599cb6d1b63d6593355.

--- client/src/modules/invoices/components/InvoiceManagementPage.tsx
+++ client/src/modules/invoices/components/InvoiceManagementPage.tsx
@@ -218,7 +218,7 @@ const InvoiceManagementPage: React.FC = () => {
   const handleExportToExcel = async () => {
     setIsExporting(true);
     try {
-      const blob = await invoiceService.exportInvoices();
+      const blob = await invoiceService.exportInvoices(page, limit, statusFilter || undefined);
 
       // Create download link
       const url = window.URL.createObjectURL(blob);
